6d3203abc78e498a6d711be3c7d458678b4e14e9
Jeremy Meyer <jjman505@gmail.com>
2016-01-18 21:21:30 -0500

    updated hompage bg header

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,10 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/header.png': 'center'
+							// 'images/bg01.jpg': 'center',
+							// 'images/bg02.jpg': 'center',
+							// 'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
