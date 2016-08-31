6e0a2dddfd3d67b8bce6199fa726d7259c62ff81
Michael Goldman <goldmandrummer@gmail.com>
2016-08-30 23:05:31 -0400

    swap splash for new main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,159 +1,100 @@
 /*
-  Eventually by HTML5 UP
-  html5up.net | @n33co
-  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function() {
+(function($) {
 
-  "use strict";
+	skel
+		.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
 
-  // Methods/polyfills.
+	$(function() {
 
-    // classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-      !function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
+			$banner = $('#banner'),
+			$header = $('#header');
 
-    // canUse
-      window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-    // window.addEventListener
-      (function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
 
-  // Vars.
-    var $body = document.querySelector('body');
+		// Mobile?
+			if (skel.vars.mobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
+					});
 
-  // Disable animations/transitions until everything's loaded.
-    $body.classList.add('is-loading');
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
 
-    window.addEventListener('load', function() {
-      window.setTimeout(function() {
-        $body.classList.remove('is-loading');
-      }, 100);
-    });
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
 
-  // Slideshow Background.
-    (function() {
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					speed: 1500,
+					offset: $header.outerHeight()
+				});
 
-      // Settings.
-        var settings = {
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
 
-          // Images (in the format of 'url': 'alignment').
-            images: {
-              'images/header.png': 'center'
-              // 'images/bg01.jpg': 'center',
-              // 'images/bg02.jpg': 'center',
-              // 'images/bg03.jpg': 'center'
-            },
+		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
 
-          // Delay.
-            delay: 6000
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
 
-        };
+				$window.on('resize', function() { $window.trigger('scroll'); });
 
-      // Vars.
-        var pos = 0, lastPos = 0,
-          $wrapper, $bgs = [], $bg,
-          k, v;
+				$banner.scrollex({
+					bottom:		$header.outerHeight() + 1,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
 
-      // Create BG wrapper, BGs.
-        $wrapper = document.createElement('div');
-          $wrapper.id = 'bg';
-          $body.appendChild($wrapper);
+			}
 
-        for (k in settings.images) {
+	});
 
-          // Create BG.
-            $bg = document.createElement('div');
-              $bg.style.backgroundImage = 'url("' + k + '")';
-              $bg.style.backgroundPosition = settings.images[k];
-              $wrapper.appendChild($bg);
-
-          // Add it to array.
-            $bgs.push($bg);
-
-        }
-
-      // Main loop.
-        $bgs[pos].classList.add('visible');
-        $bgs[pos].classList.add('top');
-
-        // Bail if we only have a single BG or the client doesn't support transitions.
-          if ($bgs.length == 1
-          ||  !canUse('transition'))
-            return;
-
-        window.setInterval(function() {
-
-          lastPos = pos;
-          pos++;
-
-          // Wrap to beginning if necessary.
-            if (pos >= $bgs.length)
-              pos = 0;
-
-          // Swap top images.
-            $bgs[lastPos].classList.remove('top');
-            $bgs[pos].classList.add('visible');
-            $bgs[pos].classList.add('top');
-
-          // Hide last image after a short delay.
-            window.setTimeout(function() {
-              $bgs[lastPos].classList.remove('visible');
-            }, settings.delay / 2);
-
-        }, settings.delay);
-
-    })();
-
-  // Signup Form.
-  (function() {
-
-    // Vars.
-    var $form = document.querySelectorAll('#signup-form')[0],
-      $submit = document.querySelectorAll('#signup-form button')[0],
-      $input = document.querySelectorAll('#signup-form input')[0],
-      original_button_text = $submit.innerHTML;
-
-    // Bail if addEventListener isn't supported.
-    if (!('addEventListener' in $form)) {
-      return;
-    }
-
-    // Events.
-    $submit.addEventListener('click', function(event) {
-
-      event.stopPropagation();
-      event.preventDefault();
-      var submit_text = $input.value;
-
-      // Hide message.
-      $submit.innerHTML = '<i class="fa fa-circle-o-notch fa-spin"></i>';
-
-      // Disable submit.
-      $submit.disabled = true;
-
-      // Process form.
-      // TODO: How do we capture emails?
-
-      // Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-      // but there's enough here to piece together a working AJAX submission call that does.
-      window.setTimeout(function() {
-
-        // Reset form.
-        $form.reset();
-
-        // Enable submit.
-        $submit.disabled = false;
-        $submit.innerHTML = original_button_text;
-
-        // Show message.
-        // $message._show('success', 'Thank you!');
-        // $message._show('failure', 'Something went wrong. Please try again.');
-
-      }, 1000);
-
-    });
-
-  })();
-
-})();
+})(jQuery);
