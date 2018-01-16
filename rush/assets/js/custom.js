71f8a6261d13e4218bb733541c60c07ffd6b4eb0
Shiraz Chokshi <shirazchokshi@pal-nat186-141-128.itap.purdue.edu>
2018-01-16 15:46:18 -0500

    added new website

--- rush/assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  console.log('blah');
-  window.setTimeout(function () {
-    $('.ghost').css('visibility', 'visible')
-               .animate({ opacity: 1.0 }, 1000);
-  }, 1500);
-});
